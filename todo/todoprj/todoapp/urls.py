7fb58c2e8edeaae041eb0d046074816dfae79a2f
Babak Chalaki <168085103+Babak-Chalacki@users.noreply.github.com>
2024-09-29 18:37:49 +0330

    update

--- todo/todoprj/todoapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.home,name='home-page'),
-    path('register/',views.register,name='register'),
-    path('login/',views.login,name='login')
-]
